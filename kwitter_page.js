b996318f966df638d28881f8fbfd83eaf010304c
Adarsh23-pixel <76739763+Adarsh23-pixel@users.noreply.github.com>
2021-05-13 09:00:08 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,16 @@
+//YOUR FIREBASE LINKS
+var firebaseConfig = {
+      apiKey: "AIzaSyAs_l53cNLO8R0nzDUlDqCSGNCIOmBiP80",
+      authDomain: "kwitter-28a43.firebaseapp.com",
+      databaseURL: "https://kwitter-28a43-default-rtdb.firebaseio.com",
+      projectId: "kwitter-28a43",
+      storageBucket: "kwitter-28a43.appspot.com",
+      messagingSenderId: "23835514181",
+      appId: "1:23835514181:web:8d67f6e179f147b6e89a2c"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    user_name = localStorage.getItem("user_name");
+    room_name = localStorage.getItem("room_name");
+
+    
